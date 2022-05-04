5894a16d4c23afdb6aaa1e1d5e66071895e7b665
haneesh9840 <dabbiruhaneesh@gmail.com>
2022-05-04 15:06:51 +0530

    new files added and also updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const bodyparser = require('body-parser')
 var lc = require("./leetcode.js");
 var gfg = require("./gfg.js");
+var gfg2 = require("./gfg2.js");
 app.use(express.static("public"))
 
 app.set('view engine', 'ejs')
@@ -17,10 +18,13 @@ var userdetails = new mongoose.Schema({
     gfgprofilename: String,
     lcprofilename: String,
     mtarget: Number,
-    dtarget: Number
+    dtarget: Number,
+    tgfgsolved: Number,
+    tlcsolved: Number
 })
 
 var Userdetails = mongoose.model('Userdetails', userdetails)
+
 app.get("/login", (req, res) => {
     res.render("login")
 })
@@ -28,51 +32,120 @@ app.get("/register", (req, res) => {
     res.render("register")
 })
 app.get("/", (req, res) => {
-    res.render("main")
+    console.log(res.body)
+    lc.letscall("dabbiruhaneesh").then((result1) => {
+            gfg.scrapeData("dabbiruhaneesh").then((result2) => {
+                gfg2.scrapeData("dabbiruhaneesh").then((result3) => {
+                    //var alpha = result3
+
+                    res.render("main", { gfg_c: result1, lc_c: result2, todaygfg: result3 })
+                        //res.render("main", { lc_c: result })
+                })
+
+            })
+
+
+        })
+        //var alpha = setTimeout(lc.letscall("dabbiruhaneesh"), 5000)
+
+
+    //console.log(alpha)
 })
 app.post('/login', (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-        //db.collection.find({_id: "myId"}, {_id: 1}).limit(1)
-    Userdetails.findOne({ "username": username, "password": { $exists: true } }, (err, founduser) => {
-            if (err)
-                console.log("sorry no user found")
-            else {
-                //console.log(founduser)
-                if (founduser !== null && founduser.password === password) {
-                    res.render('main')
-                } else {
-                    console.log('error')
-                        //console.log(founduser.password + password)
+        const username = req.body.username
+        const password = req.body.password
+            //db.collection.find({_id: "myId"}, {_id: 1}).limit(1)
+        Userdetails.findOne({ "username": username, "password": { $exists: true } }, (err, founduser) => {
+                if (err)
+                    console.log("sorry no user found")
+                else {
+                    //console.log(founduser)
+                    if (founduser !== null && founduser.password === password) {
+                        lc.letscall(founduser.lcprofilename).then((result1) => {
+                            gfg.scrapeData(founduser.gfgprofilename).then((result2) => {
+
+                                res.render('main', { gfg_c: result2, lc_c: result1, todaygfg: result2 - founduser.tgfgsolved, todaylc: result1 - founduser.tlcsolved, uname: founduser.username })
+                            })
+                        })
+                    } else {
+                        console.log('error')
+                            //console.log(founduser.password + password)
+                    }
+
                 }
-
             }
-        }
 
-    )
-
+        )
 
 
-})
+
+    })
+    //-----------------problems solved schema----------------------
+
+let t1 = new Array(12).fill(0).map(() => new Array(31).fill(0));
+let t2 = new Array(12).fill(0).map(() => new Array(31).fill(0));
+let t3 = new Array(12).fill(0).map(() => new Array(31).fill(0));
+var problems = new mongoose.Schema({
+    username: String,
+    email: String,
+    gfgsolved: { type: Array, default: t1 },
+    leetcodesolved: { type: Array, default: t2 },
+    manualsolved: { type: Array, default: t3 },
+});
+
+var problems_s = mongoose.model("problems_s", problems);
+//-----------------problems solved schema----------------------
 app.post('/register', (req, res) => {
-    const newuser = new Userdetails({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        gfgprofilename: req.body.gfg,
-        lcprofilename: req.body.lc,
-        mtarget: req.body.mtarget,
-        dtarget: req.body.dtarget
 
+        //----------------------------
+
+        var obj = new problems_s({
+            username: req.body.username,
+            email: req.body.email,
+            gfgsolved: t1,
+            leetcodesolved: t2,
+            manualsolved: t3,
+        });
+        obj.save();
+
+        //console.log(newuser[dtarget])
+
+
+        lc.letscall(req.body.lc).then((result1) => {
+            gfg.scrapeData(req.body.gfg).then((result2) => {
+                gfg2.scrapeData(req.body.gfg).then((result3) => {
+                    //var alpha = result3
+
+                    const newuser = new Userdetails({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: req.body.password,
+                        gfgprofilename: req.body.gfg,
+                        lcprofilename: req.body.lc,
+                        mtarget: req.body.mtarget,
+                        dtarget: req.body.dtarget,
+                        tgfgsolved: result2,
+                        tlcsolved: result1
+
+
+
+                    })
+                    newuser.save()
+                    res.render("main", { gfg_c: result1, lc_c: result2, todaygfg: 0, todaylc: 0, uname: req.body.username })
+                })
+
+
+                //res.render("main", { lc_c: result })
+            })
+
+        })
+
+
+
+
+        //res.render("success")
     })
-    newuser.save((err) => {
-        if (err)
-            console.log(err)
-        else
-            res.render("success")
-    })
-})
-console.log(lc.letscall("dabbiruhaneesh"))
-console.log(lc.letscall("dabbiruhaneesh").ans)
-console.log(gfg.scrapeData("dabbiruhaneesh"))
+    //console.log(Userdetails.find({}))
+
+
 app.listen(3000, (req, res) => console.log("runnning on port 3000"));
